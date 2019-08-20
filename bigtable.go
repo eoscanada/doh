f7a5f43485b7b5f597b926c33c6273e57059241c
Julien Cassis <juliencassis@gmail.com>
2019-08-20 16:25:05 -0400

    Added big table support

---
+++ bigtable.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"os"
+	"strings"
+
+	"cloud.google.com/go/bigtable"
+	"google.golang.org/api/option"
+)
+
+const emulatorHostDefault = "BIGTABLE_EMULATOR_HOST"
+const emulatorDefaultHostValue = "localhost:8086"
+
+func newBigTableClient(project, instance string, opts ...option.ClientOption) (*bigtable.Client, error) {
+	ctx := context.Background()
+	if isTestEnv(project, instance) && (os.Getenv(emulatorHostDefault) == "") {
+		os.Setenv(emulatorHostDefault, emulatorDefaultHostValue)
+	}
+
+	client, err := bigtable.NewClient(ctx, project, instance, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+func newBigTableAdminClient(project, instance string, opts ...option.ClientOption) (*bigtable.AdminClient, error) {
+	ctx := context.Background()
+	if isTestEnv(project, instance) && (os.Getenv(emulatorHostDefault) == "") {
+		os.Setenv(emulatorHostDefault, emulatorDefaultHostValue)
+	}
+
+	client, err := bigtable.NewAdminClient(ctx, project, instance, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+func isTestEnv(project, instance string) bool {
+	return (strings.HasPrefix(project, "test") || strings.HasPrefix(instance, "dev"))
+}
